16e55eff7ba14a37fdb8575debcd8d144ca776d8
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2017-04-12 04:33:10 +0000

    use simpler licence

--- hershey_text.py
+++ hershey_text.py
@@ -3,18 +3,8 @@
 # object into a collection of curves. Also needs HersheyPy
 # <https://github.com/ldo/hersheypy> to be installed.
 #
-# Copyright 2015 Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
-#
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# Copyright 2015, 2017 Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+# Licensed under CC-BY-SA <http://creativecommons.org/licenses/by-sa/4.0/>.
 #-
 
 import math
